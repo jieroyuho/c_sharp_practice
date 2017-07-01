096af85f8639b6eef4c1f89ed242f3d1b25fbb20
Tsung-Yu Ho <jieroyuho@gmail.com>
2017-07-01 17:54:14 +0800

    Tree

---
+++ c_sharp_practice/Tree01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tree01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node root = new Node() { value = 1 };
+            Node c1 = new Node() { value = 2 };
+            Node c2 = new Node() { value = 3 };
+
+            root.AddChild(c1);
+            c1.AddChild(c2);
+           
+            Console.WriteLine("{0}", root.GetNodeHeight() );
+            Console.WriteLine("{0}", c1.GetNodeHeight() );
+            Console.WriteLine("{0}", c2.GetNodeHeight() );
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ c_sharp_practice/Tree01/TreeLibrary.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tree01
+{
+    class Node
+    {
+        
+        public Node parent { get; set; }
+        public List<Node> children { get; set; }
+        public int value { get; set; }
+
+        public Node()
+        {
+            //this.children = new List<Node>();
+        }
+        //public Node(int key)
+        //{
+        //    this.value = key;
+        //    this.children = new List<Node>();
+        //}
+
+        public void AddChild (Node childnode)
+        {
+            if (this.children == null)
+            {
+                this.children = new List<Node>();
+            }
+            this.children.Add(childnode);
+            childnode.parent = this;
+        }
+
+        public int GetNodeHeight()
+        {
+            if(children == null)
+            {
+                return 1;
+            }
+            int Max = 0;
+            foreach (Node item in this.children)
+            {
+                //Console.WriteLine("Node[{0}] Get")
+
+                if (item.GetNodeHeight() > Max)
+                {
+                    Max = item.GetNodeHeight();
+                }
+            }
+            return Max + 1;
+        }
+
+
+        public static void AddLink(Node pNode, Node cNode)
+        {
+            pNode.children.Add(cNode);
+            cNode.parent = pNode;
+        }
+
+
+    }
+
+}
